c198fed71506a6b2b489b9b33665617b416fb846
adantristan-ui <adantristan@hotmail.com>
2026-02-22 16:16:45 -0600

    Add main class with student instantiation and methods

--- Main.java
+++ Main.java
@@ -1,1 +1,31 @@
+/**
+ * Clase principal del programa
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Instanciación de objetos
+        Estudiante estudiante1 = new Estudiante(
+                "Adan Tristan",
+                "A12345",
+                85,
+                90,
+                78
+        );
+
+        Estudiante estudiante2 = new Estudiante(
+                "Maria Lopez",
+                "B67890",
+                60,
+                65,
+                70
+        );
+
+        // Uso de métodos
+        estudiante1.mostrarInformacion();
+        System.out.println("---------------------");
+        estudiante2.mostrarInformacion();
+    }
+}
 
